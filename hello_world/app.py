606d00517181bf04d52cffebffd740590a8779d7
Mohamed Benighil <mohamed.benighil07@gmail.com>
2025-12-24 09:31:12 +0100

    Changed return message

--- hello_world/app.py
+++ hello_world/app.py
@@ -37,7 +37,7 @@ def lambda_handler(event, context):
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello mohamed",
+                "message": "i'm using canary deployments",
                 # "location": ip.text.replace("\n", "")
             }
         ),
